0cdc48da7849e7968c8d03f58d15eed2f8f9d7b1
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-19 03:58:49 +0000

    Answer to task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print numbers from base 10 using putchar
+ * Description: prints numbers
+ * and the prints is 0-9
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int a = 48;
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+putchar(10);
+return (0);
+}
