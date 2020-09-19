79f5c343030f2a0ec8871f4dc9bb7bff07979689
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-19 03:43:36 +0000

    Answer to task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print numbers from base 10
+ * Description: prints numbers
+ * and the prints is 0-9
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int a = 0;
+for (a = 0; a <= 9; a++)
+{
+printf("%d", a);
+}
+putchar(10);
+return (0);
+}
