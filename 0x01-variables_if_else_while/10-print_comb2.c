4aef36e6f7529a61d31c8eebbc7817af3bd1b76e
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-19 04:57:16 +0000

    answer task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print numbers with space
+ * Description: prints numbers
+ * and the prints is 0-9
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int a = 0;
+int b = 0;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+{
+putchar(a);
+putchar(b);
+if (b < 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar(10);
+return (0);
+}
