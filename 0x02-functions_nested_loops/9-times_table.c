8dd32bf5737de9caec99e2a6079260e60ae83131
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-23 17:13:17 +0000

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,39 +4,54 @@
 */
 void times_table(void)
 {
-    int i, m, j;
-    for (i = 0; i < 10; i++)
-    {
-        for (m = 0; m < 10; m++)
-        {
-            j = m * i;
-            if (j < 10)
-            {
-                _putchar(j + '0');
-            }
-            else
-            {
-                _putchar(j / 10 + '0');
-                _putchar(j % 10 + '0');
-            }
-            if (m < 9)
-            {
-                _putchar(44);
-                if (j < 10)
-                {
-                    _putchar(32);
-                    _putchar(32);
-                }
-                else 
-                {
-                    _putchar(32);
-                }
-            }
-            
-            if (m == 9)
-            {
-                _putchar(10);
-            }
-        }
-    }
+int factor1, factor2, result;
+for (factor1 = 0; factor1 < 10; factor1++)
+{
+for (factor2 = 0; factor2 < 10; factor2++)
+{
+if (factor2 == 0)
+{
+_putchar('0');
+_putchar(44);
+}
+else
+{
+result = factor1 * factor2;
+if (factor2 == 9)
+{
+if (result > 9)
+{
+_putchar(32);
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+_putchar(10);
+}
+else
+{
+_putchar(32);
+_putchar(32);
+_putchar(result + '0');
+_putchar(10);
+}
+}
+else
+{
+if (result > 9)
+{
+_putchar(32);
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+_putchar(44);
+}
+else
+{
+_putchar(32);
+_putchar(32);
+_putchar(result + '0');
+_putchar(44);
+}
+}
+}
+}
+}
 }
