68426ace7003f04aaf9268399ef31942f0d9cd72
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-24 02:28:26 +0000

    infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+/**
+ * main - i = 0; this is a commment for this code
+ * @printf: prints a text
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+ */
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
