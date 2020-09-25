e48a429f6cc979072b7408dbea9679de1d3bda27
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-25 19:16:40 +0000

    Answer to task 3 - debugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _isupper - this return 1 if is uppercase
+* or 0 if is lowercase
+* @c: The character to evaluate
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _isupper(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
