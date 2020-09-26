1eed34a01235727482e2dd4a4bf8da5d6d328ca3
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-26 23:34:09 +0000

    Answer to task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* mul - returns the result of a multiplication
+* using the variables a and b.
+* @a: The digit to multiplicate.
+* @b: the digit to multiplicate.
+ * Return: the result of the multiplication
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
