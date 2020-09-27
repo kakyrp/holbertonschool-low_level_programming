f9de8cb3d427619944948543c88f3fe18737d188
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-27 00:21:39 +0000

    Answer to task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints numbers
+ * except 2 and 4.
+ */
+void print_most_numbers(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+if (c != 50 && c != 52)
+{
+_putchar (c);
+}
+}
+_putchar (10);
+}
