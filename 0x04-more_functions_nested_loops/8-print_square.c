6d5b3fe51ff45150aa839cf1a07bf7d2e51a8a8c
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-28 04:31:04 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square using #.
+ * @size: The size of the square.
+ */
+void print_square(int size)
+{
+    int k;
+    int j;
+
+    if (size > 0)
+    {
+        for (k = 0; k < size; k++)
+        {
+            for (j = 0; j < size; j++)
+            {
+                _putchar(35);
+            }
+            _putchar (10);
+        }
+    }
+    else
+    {
+        _putchar(10);
+    }
+}
