7b6860b9f605572878bd50f2747a53b35df09e11
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-28 02:50:36 +0000

    Task 6_print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - print the lines requested.
+ * @n: the number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+int k;
+if (n > 0)
+{
+for (k = 0; k <= n; k++)
+{
+_putchar (95);
+}
+}
+_putchar (10);
+}
