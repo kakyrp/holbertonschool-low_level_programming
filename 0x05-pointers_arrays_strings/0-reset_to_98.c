74c43dbb0d55e49ecfa893c066320e9c342e3688
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-30 01:40:57 +0000

    First pointer commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - function that changes the address for the variable n.
+ * @n: the pointer to assign a new value.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
