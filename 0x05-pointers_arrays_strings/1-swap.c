c3a782f99f337a6727bb7863fb52f9cfacdbd67a
karodri.p@gmail.com <karodri.p@gmail.com>
2020-09-30 01:55:36 +0000

    Answer to task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * swap_int - function that changes the address for the variable n.
+ * @a: the pointer to assign a new value.
+ * @b: the second pointer to assign a new value.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
