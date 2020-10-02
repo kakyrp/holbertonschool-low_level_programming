de49e633fe59814babcb2589de593c6b9f08b4c2
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-02 22:18:17 +0000

    Answer to task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,6 +7,22 @@
  * Return: a pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
-{
 
+{
+int l = 0, i = 0;
+while (dest[l] != '\0')
+{
+l++;
+i++;
+}
+*(dest + l + 1) = ' ';
+i = 0;
+
+while (src[i] != '\0')
+{
+*(dest + (l + i)) =  src[i];
+i++;
+}
+*(dest + (l + i + 1)) = '\0';
+return (dest);
 }
