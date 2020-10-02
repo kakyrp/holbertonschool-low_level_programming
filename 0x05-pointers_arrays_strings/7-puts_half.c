3b95370c43bf0eeee93de26e79963ba4877cfe39
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-02 01:47:15 +0000

    Answer to task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * puts_half - the function that prints the half of the array.
+ * @str: The variable / string to print.
+ */
+void puts_half(char *str)
+{
+int i, a, b;
+
+for (i = 0; str[i] != '\0';)
+{
+i++;
+}
+a = i - 1;
+b = (a / 2);
+if (((a + 1) % 2) == 0)
+{
+    b = ((a + 2 ) / 2);
+}
+else
+{
+    b = ((a + 2) / 2);
+}
+for (i = b; i <= a; i++)
+{
+_putchar(str[i]);
+}
+_putchar(10);
+}
