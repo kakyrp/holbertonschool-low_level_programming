96fa1ec68cff149edf65ee97060b9ec3ece4e577
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-02 03:31:22 +0000

    Uploading files

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * puts2 - the function that will print every other line.
---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strcat - function that concatenates two strings.
+ * @src: The first string.
+ * @dest: The second string.
+ * Return: a pointer to the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+
+}
