d0c56351b247e283ea8ce8c05ae472d4756a78ea
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-02 01:26:04 +0000

    Answer to task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * puts2 - the function that will print every other line.
+ * @str: The variable / string to count.
+ */
+void puts2(char *str)
+{
+int i, a;
+
+for (i = 0; str[i] != '\0';)
+{
+i++;
+}
+a = i - 1;
+
+for (i = 0; i <= a; i += 2)
+{
+_putchar(str[i]);
+}
+putchar(10);
+}
