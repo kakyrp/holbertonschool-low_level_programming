163dee02eeacdc07bda20e02f7a76105bfb4e358
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-23 19:57:49 +0000

    prime number file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+int recursion(int index, int final);
+/**
+ * is_prime_number - function that returns 1 if the input integer is a prime number
+ * otherwise return 0.
+ * @n: The number used.
+ * Return: 1 if n is a prime number, otherwise return is always: 0.
+ */
+int is_prime_number(int n)
+{
+    if (n <= 0)
+    {
+    return (0);
+    }
+    else if (n < 2)
+    {
+        return (0);
+    }
+    else
+    {
+        return (recursion(2, n));
+    }
+}
+/**
+ * recursion - auxiliar function
+ * @index: represents the number 2.
+ * @final: represents the n number.
+ * Return: 1 if n is a prime number, otherwise return is always: 0.
+ */
+int recursion(int index, int final)
+{
+    if (index % final == 0 && final != index)
+    {
+        return (0);
+    }
+    else if (final == index)
+    {
+        return (1);
+    }
+    else
+    {
+        return (recursion(final, ++index));
+    }
+}
