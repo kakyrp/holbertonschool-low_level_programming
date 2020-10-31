486faa25365cf3bb2acdc645017daee5d65d48ba
karodri.p@gmail.com <karodri.p@gmail.com>
2020-10-31 23:19:11 +0000

    Print string answer

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_string - function that prints strings, followed by a new line
+ * @separator: the char array to use.
+ * @n: numbers given.
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list mylist;
+	unsigned int i = 0;
+	unsigned int index = 0;
+	va_start(mylist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		index++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+		{
+			printf("%s", va_arg(mylist, char *));
+		}
+		else
+		{
+			if (i < index - 1)
+			{
+			printf("%s%s", va_arg(mylist, char *), separator);
+			}
+			else
+			{
+				printf("%s", va_arg(mylist, char *));
+			}
+		}
+	}
+	printf("\n");
+	va_end(mylist);
+}
