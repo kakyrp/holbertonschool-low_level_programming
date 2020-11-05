9afc05947c285d176a4709c0f0bf56ba2e886822
karodri.p@gmail.com <karodri.p@gmail.com>
2020-11-05 23:08:42 +0000

    Uploading headers, README and task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "holberton.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: pointer to the head of linked lists.
+ * return: number of elements in list_t.
+*/
+size_t print_list(const list_t *h)
+{
+	unsigned int elements = 0;
+	while (h != NULL)
+	{
+		if (h->str != 0)
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
+
---
+++ 0x12-singly_linked_lists/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h);
+
+#endif
