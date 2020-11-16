dc0f60935462182990278e022db4d4f8a5ddbda5
karodri.p@gmail.com <karodri.p@gmail.com>
2020-11-16 21:28:16 +0000

    Uploading file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * prototype - template
+ * @void: Counts the number of parameters that go into main.
+ * Return: Always 0 on success.
+ */
+void prototype(void)
+{
+
+}
