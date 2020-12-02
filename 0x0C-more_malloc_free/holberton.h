dc0fa7d360f6bf4614ff9e4544ce6c631b39d65e
karodri.p@gmail.com <karodri.p@gmail.com>
2020-12-02 04:16:43 +0000

    uploading task 100

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -9,6 +9,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 char *memorySet(char *ptr, int x, unsigned int n);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char memoryCopy(char *dest, char *src, unsigned int n);
 
 
 #endif
