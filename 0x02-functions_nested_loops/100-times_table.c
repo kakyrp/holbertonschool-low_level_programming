45b56b7134e09bd5126ca033fc60931e8cbd73f6
karodri.p@gmail.com <karodri.p@gmail.com>
2020-12-03 06:02:33 +0000

    Fixing code in task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,45 +3,55 @@
 /**
  * print_times_table - prints the n times table starting with 0 betweetn 0-15
  * @n: integer
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Nothing
  */
 void print_times_table(int n)
 {
-	int i, j, m, d;
+	int i, j, nm;
 
-	if ((n < 15) && (n >= 0))
+	if ( n < 0 || n > 15)
+	{
+		return;
+	}
+	else
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				d = i * j;
-				if (j != 0)
+				nm = i * j;
+				if (nm < 10)
+				{
+					_putchar(nm + 48);
+				}
+				else if (nm < 100)
+				{
+					_putchar((nm / 10) + 48);
+					_putchar((nm % 10) + 48);
+				}
+				else
+				{
+					_putchar((nm / 100) + 48);
+					_putchar(((nm / 10) % 10) + 48);
+					_putchar((nm % 10) + 48);
+				}
+
+				if (j != n)
 				{
 					_putchar(44);
 					_putchar(32);
-					if (d <= 9)
-					{
-						_putchar(32);
-						_putchar(32);
-					}
 				}
-
-				if (d > 99)
-				{
-					m = d / 10;
-					_putchar('0' + m / 10);
-					_putchar('0' + m % 10);
-				}
-				else if ((d >= 10) && (d <= 99))
+				if ((i * (j + 1)) < 10 && (j != n))
 				{
 					_putchar(32);
-					_putchar('0' + d / 10);
+					_putchar(32);
 				}
-				_putchar('0' + d % 10);
+				else if ((i * (j + 1)) < 100 && (j != n))
+				{
+					_putchar(32);
+				}
 			}
-			_putchar('\n');
+			_putchar(10);
 		}
 	}
 }
