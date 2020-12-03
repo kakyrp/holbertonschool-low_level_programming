35fb5e10e4f535617e6669235baffcd5c80186f4
karodri.p@gmail.com <karodri.p@gmail.com>
2020-12-03 02:14:14 +0000

    Uploading header with new structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef STRUCTS
+#define STRUCTS
+/**
+ * struct dog - Structure for dog description
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: Longer description.
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
+#endif
