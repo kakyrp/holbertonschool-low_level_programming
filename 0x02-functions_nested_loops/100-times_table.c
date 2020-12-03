dd0f0d15596a69265b5f648f737bf884776518b2
karodri.p@gmail.com <karodri.p@gmail.com>
2020-12-03 06:18:10 +0000

    Fixing problem with code in github

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,36 +7,37 @@
  */
 void print_times_table(int n)
 {
-	int i, j, nm;
+	int i, j, m, d;
 
-	if (n < 0 || n > 15)
-	{
-		return;
-	}
-	else if
+	if ((n < 15) && (n >= 0))
 	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				nm = i * j;
-				if (nm < 10)
-					_putchar(nm + 48);
-				else if (nm < 100)
-					_putchar((nm / 10) + 48);
-					_putchar((nm % 10) + 48);
-				else
-					_putchar((nm / 100) + 48);
-					_putchar(((nm / 10) % 10) + 48);
-					_putchar((nm % 10) + 48);
-				if (j != n)
+				d = i * j;
+				if (j != 0)
+				{
 					_putchar(44);
 					_putchar(32);
-				if ((i * (j + 1)) < 10 && (j != n))
+					if (d <= 9)
+					{
+						_putchar(32);
+						_putchar(32);
+					}
+				}
+				if (d > 99)
+				{
+					m = d / 10;
+					_putchar('0' + m / 10);
+					_putchar('0' + m % 10);
+				}
+				else if ((d >= 10) && (d <= 99))
+				{
 					_putchar(32);
-					_putchar(32);
-				else if ((i * (j + 1)) < 100 && (j != n))
-					_putchar(32);
+					_putchar('0' + d / 10);
+				}
+				_putchar('0' + d % 10);
 			}
 			_putchar(10);
 		}
