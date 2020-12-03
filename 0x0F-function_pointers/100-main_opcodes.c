a40f05965bb5b72ad8894fb438f7024bdd4ef217
karodri.p@gmail.com <karodri.p@gmail.com>
2020-12-03 05:01:46 +0000

    uploading task 100-main opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function.
+ * @argc: Counts the number of parameters that go into main.
+ * @argv: Pointer of array of pointers containing strings entering main.
+ * Return: 0 on success, 1 on argv != 2, 2 on negative bytes in argv.
+ */
+int main(int argc, char **argv)
+{
+	int i, bytes;
+	char *mainadd;
+
+	if (argc != 2)
+	{
+		printf("Error\n"), exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n"), exit(2);
+	}
+
+	mainadd = (char *)main;
+
+	for (i = 0; i < bytes - 1; i++)
+	{
+		printf("%02hhx ", mainadd[i]);
+	}
+	printf("%20hhx\n", mainadd[i]);
+	return (0);
+}
